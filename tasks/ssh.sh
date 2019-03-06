5faeec06ec9974bacb226b21b608ba85df400163
davemackintosh <me@davemackintosh.co.uk>
2019-03-06 13:40:44 +0000

    Write to the right place

--- tasks/ssh.sh
+++ tasks/ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
-test ! -f "$HOME/.ssh/id_rsa.pub" && ssh-keygen -f id_rsa -t rsa -N '' &&
-echo "\nUpload your ssh key to the relevant services:\n" &&
+test ! -f "$HOME/.ssh/id_rsa.pub" && ssh-keygen -f id_rsa -t rsa -N '' -f "$HOME/.ssh/id_rsa.pub" &&
+echo -e "\nUpload your ssh key to the relevant services:\n" &&
 cat "$HOME/.ssh/id_rsa.pub"
