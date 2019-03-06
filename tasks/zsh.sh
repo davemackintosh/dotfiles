a6d6ef6e99f612816bcb71bb9fc4750ddcd48a3f
davemackintosh <me@davemackintosh.co.uk>
2019-03-06 17:24:06 +0000

    Copy the aliases file over too

--- tasks/zsh.sh
+++ tasks/zsh.sh
@@ -3,4 +3,6 @@
 wget -O "$HOME/z.sh" https://raw.githubusercontent.com/rupa/z/master/z.sh &&
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k || true &&
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || true &&
-cp .zshrc "$HOME/.zshrc"
+cp .zshrc "$HOME/.zshrc" &&
+cp ../aliases.sh "$HOME/"
+
