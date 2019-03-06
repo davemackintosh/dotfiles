1a09177c5683680e4e0cdc40ce9e70c379e18062
davemackintosh <me@davemackintosh.co.uk>
2019-03-06 13:33:59 +0000

    Blasted git

--- tasks/nvim.sh
+++ tasks/nvim.sh
@@ -2,7 +2,7 @@
 
 pip install neovim &&
 npm i -G neovim &&
-[[ ! -d ~/.vim/bundle/Vundle.vim ]] && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &&
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || true &&
 cp ../.ctags "$HOME/" &&
 cp ../.vimrc "$HOME/" &&
 mkdir -p "$HOME/.config" &&
