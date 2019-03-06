deb8c847bd6ec57f07983c56c7158713b9bcbbbf
davemackintosh <me@davemackintosh.co.uk>
2019-03-06 15:09:27 +0000

    This should be the nail in the coffin

--- android.sh
+++ android.sh
@@ -3,7 +3,6 @@
 termux-setup-storage &&
 ./tasks/apt.sh &&
 ./tasks/zsh.sh &&
-sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)" &&
 ./tasks/npm.sh &&
 ./tasks/nvim.sh &&
 source "$HOME/.zshrc" &&
--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 echo "Running install for $OS"
 
 if [[ "$ANDROID_ROOT" = "/system"  ]]; then
+  sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)" &&
   ./android.sh
 elif [[ "$OS" = "Linux" ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
   ./linux.sh
 fi
 
