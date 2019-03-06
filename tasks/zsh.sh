b20504378ac4fe8ecc8cdd1e595d0a5f72b2657b
davemackintosh <me@davemackintosh.co.uk>
2019-03-06 13:22:50 +0000

    Check if it exists before trying to clone it

--- tasks/zsh.sh
+++ tasks/zsh.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
 wget -O "$HOME/z.sh" https://raw.githubusercontent.com/rupa/z/master/z.sh &&
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
+[[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
 cp .zshrc "$HOME/.zshrc"
