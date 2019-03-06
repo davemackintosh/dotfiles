dd31540ddd3fb2bcdcfc7ccb4f989fead0b09944
davemackintosh <me@davemackintosh.co.uk>
2019-03-06 13:35:42 +0000

    PATHS

--- tasks/nvim.sh
+++ tasks/nvim.sh
@@ -3,8 +3,8 @@
 pip install neovim &&
 npm i -G neovim &&
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim || true &&
-cp ../.ctags "$HOME/" || true &&
-cp ../.vimrc "$HOME/" || true &&
+cp .ctags "$HOME/" || true &&
+cp .vimrc "$HOME/" || true &&
 mkdir -p "$HOME/.config" &&
 cp -R .config/nvim "$HOME/.config" &&
 nvim +UpdateRemotePlugins +qall &&
