68a7edaec171f528e9e77022f07476d1365798ed
davemackintosh <me@davemackintosh.co.uk>
2019-03-06 13:25:22 +0000

    Ignore errors here

--- tasks/zsh.sh
+++ tasks/zsh.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
 wget -O "$HOME/z.sh" https://raw.githubusercontent.com/rupa/z/master/z.sh &&
-[[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || true &&
 cp .zshrc "$HOME/.zshrc"
