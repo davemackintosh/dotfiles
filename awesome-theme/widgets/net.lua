6b56278c5dcdd022c79d6c6967a7b802c15287b9
davemackintosh <me@davemackintosh.co.uk>
2019-05-21 14:04:19 +0100

    Added ethernet icon

--- awesome-theme/widgets/net.lua
+++ awesome-theme/widgets/net.lua
@@ -11,35 +11,40 @@ local net = lain.widget.net {
     wifi_state = "on",
     eth_state = "on",
     settings = function()
-        local eth0 = net_now.devices.eth0
-        if eth0 then
-            if eth0.ethernet then
-                eth_icon:set_image(ethernet_icon_filename)
-            else
-                eth_icon:set_image()
-            end
+      local eth0 = net_now.devices.enp14s0u1u2
+      if eth0 then
+        if eth0.ethernet then
+          print(icons.ethernetConnected)
+          eth_icon:set_image(icons.ethernetConnected)
+        else
+          eth_icon:set_image()
         end
+      end
 
-        local wlan0 = net_now.devices.wlp2s0
+      local wlan0 = net_now.devices.wlp2s0
 
-        if wlan0 then
-            if wlan0.wifi then
-                local signal = wlan0.signal
-                if signal < -83 then
-                    wifi_icon:set_image(icons.wifiWeak)
-                elseif signal < -70 then
-                    wifi_icon:set_image(icons.wifiMid)
-                elseif signal < -53 then
-                    wifi_icon:set_image(icons.wifiGood)
-                elseif signal >= -53 then
-                    wifi_icon:set_image(icons.wifiGood)
-                end
-            else
-                wifi_icon:set_image()
-            end
+      if wlan0 then
+        if wlan0.wifi then
+          local signal = wlan0.signal
+          if signal < -83 then
+            wifi_icon:set_image(icons.wifiWeak)
+          elseif signal < -70 then
+            wifi_icon:set_image(icons.wifiMid)
+          elseif signal < -53 then
+            wifi_icon:set_image(icons.wifiGood)
+          elseif signal >= -53 then
+            wifi_icon:set_image(icons.wifiGood)
+          end
+        else
+          wifi_icon:set_image(icons.wifiOff)
         end
+      end
     end
 }
 
-return utils.iconMargin(wifi_icon)
+return wibox.widget {
+  layout = wibox.layout.align.horizontal,
+  utils.iconMargin(wifi_icon),
+  utils.iconMargin(eth_icon)
+}
 
