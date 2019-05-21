39ec9f9fff336560e558e0333a3d4166394e4c4e
davemackintosh <me@davemackintosh.co.uk>
2019-05-21 14:05:12 +0100

    Stop this nonsense

--- awesome-theme/widgets/taglist.lua
+++ awesome-theme/widgets/taglist.lua
@@ -7,11 +7,6 @@ local vars = require("vars")
 -- More useful than you think.
 local taglist_buttons = gears.table.join(
   awful.button({ }, 1, function(t) t:view_only() end),
-  awful.button({ modkey }, 1, function(t)
-        if client.focus then
-            client.focus:move_to_tag(t)
-        end
-    end),
   awful.button({ }, 3, awful.tag.viewtoggle),
   awful.button({ modkey }, 3, function(t)
         if client.focus then
