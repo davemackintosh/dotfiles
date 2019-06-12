94b87b20ba42a5e8ec96b8b600c4f4efcdd68e13
davemackintosh <me@davemackintosh.co.uk>
2019-06-12 13:53:43 +0100

    Added my dvd backup tool.

---
+++ dvd-to-iso.sh
@@ -0,0 +1,34 @@
+#!/bin/env bash
+
+clear &&
+
+NAME="$1"
+TYPE="$2"
+
+ISONAMESPC=${NAME,}
+ISONAME=${ISONAMESPC// /-}
+
+if [ -z "$NAME" ]
+then
+  echo "Usage dvd-to-iso.sh {NAME} {Movies | TV}"
+  exit -1
+fi
+
+if [ -z "$TYPE" ]
+then
+  echo "Usage dvd-to-iso.sh $NAME {Movies | TV}"
+  exit -1
+fi
+
+mkdir -p "$HOME/Videos/$TYPE/$NAME/" &&
+
+echo -e "Pulling DVD to disc: This can take a while\n" &&
+dvdbackup -i /dev/sr0 -n "$NAME" -o "$HOME/Videos/$TYPE/" -M &&
+
+echo -e "Converting pulled files to an iso; $NAME\n"
+mkisofs -dvd-video -o "$HOME/Videos/$TYPE/$ISONAME.iso" "$HOME/Videos/$TYPE/$NAME" &&
+
+echo -e "Cleaning up\n" &&
+rm -rf "$HOME/Videos/$TYPE/$NAME/" &&
+
+echo "Done, rsync to the OSCM machine if you're at home."
