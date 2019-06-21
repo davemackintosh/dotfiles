2e0b62153998ef77bd54b61eeffa09e7051bec6d
davemackintosh <me@davemackintosh.co.uk>
2019-06-21 19:57:02 +0100

    this will probably work

--- dvd-to-iso-kodi.sh
+++ dvd-to-iso-kodi.sh
@@ -36,6 +36,17 @@ createSeasonEString() {
   return 1
 }
 
+convertDvd() {
+  if [ "$TYPE" = "tv" ]
+    then
+      KODIFRIENDLY=$(createSeasonEString $SERIES $EPSTART $EPSEND)
+
+      mkisofs -dvd-video -o "$MOUNTPOINT/$TYPE/$ISONAME.iso" "$MOUNTPOINT/$TYPE/$NAME.$KODIFRIENDLY" 
+    else
+      mkisofs -dvd-video -o "$MOUNTPOINT/$TYPE/$ISONAME.iso" "$MOUNTPOINT/$TYPE/$NAME" 
+  fi 
+}
+
 if [ -z "$NAME" ]
 then
   echo "Usage dvd-to-iso-kodi.sh {NAME} {movies | tv} season episode-num-start episode-num-end"
@@ -66,15 +77,13 @@ then
   exit -1
 fi
 
-mkdir -p "$MOUNTPOINT/$TYPE/$NAME/" &&
+mkdir -p "$MOUNTPOINT/$TYPE/$NAME/" || exit -1 &&
 
-echo -e "Pulling DVD to disc: This can take a while\n" &&
+echo -e "Pulling DVD '$NAME' to disc '$MOUNTPOINT/$TYPE/$NAME: This can take a while\n" &&
 dvdbackup -i /dev/sr0 -n "$NAME" -o "$MOUNTPOINT/$TYPE/" -M &&
 
 echo -e "Converting pulled files to an iso; $NAME\n"
-KODIFRIENDLY=$(createSeasonEString $SERIES $EPSTART $EPSEND)
-
-mkisofs -dvd-video -o "$MOUNTPOINT/$TYPE/$ISONAME.iso" "$MOUNTPOINT/$TYPE/$NAME.$KODIFRIENDLY" &&
+convertDvd &&
 
 echo -e "Cleaning up\n"
 #rm -rf "$MOUNTPOINT/$TYPE/$NAME/" &&
