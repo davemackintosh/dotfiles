b98cf2a703b373f6f107274b9fe707260286fef2
davemackintosh <me@davemackintosh.co.uk>
2019-06-24 08:36:47 +0100

    This is now a separate repo https://github.com/davemackintosh/vex

--- awesome-theme/tools/unsplash.lua
+++
@@ -1,8 +0,0 @@
-local spawn = require("awful.spawn")
-local UNSPLASH_API_KEY = os.getenv("UNSPLASH_API_KEY")
-
-if type(UNSPLASH_API_KEY) ~= nil then
-  local getRandomWallpaperCmd = "curl https://api.unsplash.com/search/photos/?client_id=" .. UNSPLASH_API_KEY .. "&query=80s | jq .results | jq 'map(.urls.full)'"
-
-  print(spawn(getRandomWallpaperCmd))
-end
--- awesome-theme/widgets/clock.lua
+++
@@ -1,7 +0,0 @@
-local wibox = require("wibox")
-local vars = require("vars")
-local utils = require("utils")
-
-local clock = wibox.widget.textclock(utils.wiBarFont("%a %d %b %H:%M%P"), 60)
-
-return clock
