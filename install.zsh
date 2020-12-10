f3c6dfbb739152d5581b25b25c7c23825abf0734
davemackintosh <me@davemackintosh.co.uk>
2020-12-10 14:43:41 +0000

    Installer now works.

---
+++ install.zsh
@@ -0,0 +1,7 @@
+#!/bin/env zsh
+
+ln -sf $HOME/dotfiles/shared/aliases.zsh $HOME/
+ln -sf $HOME/dotfiles/shared/keybindings.zsh $HOME/
+ln -sf $HOME/dotfiles/shared/.p10k.zsh $HOME/
+ln -sf $HOME/dotfiles/shared/.tmux.conf $HOME/
+ln -sf $HOME/dotfiles/shared/.gitconfig $HOME/
