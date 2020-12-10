c01043e94200a07580e19948626950241a559e32
davemackintosh <me@davemackintosh.co.uk>
2020-12-10 16:06:12 +0000

    Fixed installer and added Git signing

--- install.zsh
+++ install.zsh
@@ -1,5 +1,6 @@
 #!/bin/env zsh
 
+ln -sf $HOME/dotfiles/shared/.zshrc $HOME/
 ln -sf $HOME/dotfiles/shared/aliases.zsh $HOME/
 ln -sf $HOME/dotfiles/shared/keybindings.zsh $HOME/
 ln -sf $HOME/dotfiles/shared/.p10k.zsh $HOME/
