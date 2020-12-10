92357ea951ed35c90949189f503be175d70d7fb3
davemackintosh <me@davemackintosh.co.uk>
2020-12-10 09:31:33 +0000

    Don't need these anymore

--- tasks/apt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-# apt tasks.
-apt update && 
-apt upgrade &&
-apt install -y termux-tools ctags zsh openssh clang curl wget nodejs python python2 python-dev neovim
-
--- tasks/ssh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-test ! -f "$HOME/.ssh/id_rsa.pub" && ssh-keygen -f id_rsa -t rsa -N '' -f "$HOME/.ssh/id_rsa" &&
-echo -e "\nUpload your ssh key to the relevant services:\n" &&
-cat "$HOME/.ssh/id_rsa.pub"
