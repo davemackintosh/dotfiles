4ed2ee936b304fdce02bd3e864546a750c4497ec
davemackintosh <me@davemackintosh.co.uk>
2021-03-19 10:36:05 +0000

    Fixed paths.

--- shared/paths.zsh
+++ shared/paths.zsh
@@ -1,7 +1,8 @@
 # Editor specifics.
 export ANDROID_HOME="$HOME/Android/Sdk"
 export ANDROID_SDK_HOME="$HOME/Android/Sdk"
-export ANDROID_NDK_HOME="$ANDROID_HOME/ndk/22.0.6917172"
+export ANDROID_NDK_HOME="$ANDROID_HOME/ndk/23.0.7123448"
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-11.0.10.0.9-0.fc33.x86_64"
 
 # System paths.
 export PATH="$PATH:$HOME/node_modules/.bin"
@@ -26,3 +27,4 @@ PERL5LIB="/home/dave/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
 PERL_LOCAL_LIB_ROOT="/home/dave/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
 PERL_MB_OPT="--install_base \"/home/dave/perl5\""; export PERL_MB_OPT;
 PERL_MM_OPT="INSTALL_BASE=/home/dave/perl5"; export PERL_MM_OPT;
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
