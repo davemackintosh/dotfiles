75a210bb8797088d2602aca45d91b23e26ac6db9
davemackintosh <me@davemackintosh.co.uk>
2021-05-25 10:07:16 +0100

    This is just a stupid alias.

--- shared/aliases.zsh
+++ shared/aliases.zsh
@@ -14,14 +14,14 @@ alias ga='git add'
 alias gaa='git add -A'
 alias gir='echo "GRRRRR TYPOS 🤓" && git'
 
-alias open='xdg-open'
-
 if [[ $OS = "Android" ]]; then
   alias chcolor="$HOME/.termux/colors.sh"
   alias chfont="$HOME/.termux/fonts.sh"
   alias startPostgres='pg_ctl -D $PREFIX/var/lib/postgresql start'
   alias stopPostgres='pg_ctl -D $PREFIX/var/lib/postgresql stop'
 fi
+
+alias set-java='sudo alternatives --config java;export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::");source  ~/.zshrc'
 
 if [[ $OS = "Linux" ]]; then
   export NVM_DIR="$HOME/.nvm"
