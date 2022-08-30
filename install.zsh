45d704a0573763a2b5935020572f06f2247485ea
davemackintosh <me@davemackintosh.co.uk>
2022-08-30 17:16:55 +0100

    Tidy up a bit.

--- install.zsh
+++ install.zsh
@@ -1,12 +1,22 @@
 #!/bin/env zsh
 
-mkdir -p $HOME/.config
-ln -sf `pwd`/nvim $HOME/.config/nvim
-ln -sf `pwd`/ranger $HOME/.config/ranger
-ln -sf `pwd`/shared/.zshrc $HOME/
-ln -sf `pwd`/shared/paths.zsh $HOME/
-ln -sf `pwd`/shared/aliases.zsh $HOME/
-ln -sf `pwd`/shared/keybindings.zsh $HOME/
-ln -sf `pwd`/shared/.p10k.zsh $HOME/
-ln -sf `pwd`/shared/.tmux.conf $HOME/
-ln -sf `pwd`/shared/.gitconfig $HOME/
+set -e
+
+git submodule update --init --recursive --remote &
+wait $!
+
+ln -sf ~/dotfiles/shared/.zshrc $HOME/
+ln -sf ~/dotfiles/shared/paths.zsh $HOME/
+ln -sf ~/dotfiles/shared/aliases.zsh $HOME/
+ln -sf ~/dotfiles/shared/keybindings.zsh $HOME/
+ln -sf ~/dotfiles/shared/.p10k.zsh $HOME/
+ln -sf ~/dotfiles/shared/.tmux.conf $HOME/
+ln -sf ~/dotfiles/shared/.gitconfig $HOME/
+
+# Ask the user for their Github username
+read -p "Enter your Github username: " github_username
+git config --global user.name "$github_username"
+
+# Ask the user for their Github email
+read -p "Enter your Github email: " github_email
+git config --global user.email "$github_email"
--- shared/.zshrc
+++ shared/.zshrc
@@ -63,3 +63,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+[ -s ~/.luaver/luaver ] && . ~/.luaver/luaver
