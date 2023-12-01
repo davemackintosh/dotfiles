ad24da892d984aba255ebfb39f2fbdb122b045e4
davemackintosh <me@davemackintosh.co.uk>
2023-12-01 09:46:16 +0000

    Tidied up

--- install.zsh
+++ install.zsh
@@ -16,11 +16,13 @@ function await {
 # Pull our dependencies.
 await git submodule update --init --recursive --remote
 
-mkdir -p ~/.tmux/plugins
-if [ ! -d ~/.tmux/plugins/nord-tmux ]; then
-	await git clone https://github.com/arcticicestudio/nord-tmux.git ~/.tmux/plugins/nord-tmux
+await git clone git@github.com:davemackintosh/nvim $HOME/.config/nvim
+
+mkdir -p $HOME/.tmux/plugins
+if [ ! -d $HOME/.tmux/plugins/nord-tmux ]; then
+	await git clone https://github.com/arcticicestudio/nord-tmux.git $HOME/.tmux/plugins/nord-tmux
 else
-	await git -C ~/.tmux/plugins/nord-tmux pull
+	await git -C $HOME/.tmux/plugins/nord-tmux pull
 fi
 
 # Install some configs that live at $HOME
@@ -28,31 +30,21 @@ ln -sf ~/dotfiles/.zshrc $HOME/
 ln -sf ~/dotfiles/.gitconfig $HOME/
 ln -sf ~/dotfiles/.tmux.conf $HOME/
 ln -sf ~/dotfiles/.gitmessage $HOME/
-ln -sf ~/dotfiles/alacritty $HOME/.config/
-ln -sf ~/dotfiles/kitty $HOME/.config/
-ln -sf ~/dotfiles/hypr $HOME/.config/
-ln -sf ~/dotfiles/swaylock $HOME/.config/
-ln -sf ~/dotfiles/waybar $HOME/.config/
-ln -sf ~/dotfiles/wofi $HOME/.config/
-ln -sf ~/dotfiles/dunst $HOME/.config/
-ln -sf ~/dotfiles/waycorner $HOME/.config/
-ln -sf ~/dotfiles/wlogout $HOME/.config/
-ln -sf ~/dotfiles/ranger $HOME/.config/
 
 # Install goenv.
 if [ ! -d ~/.goenv ]; then
-	await git clone https://github.com/syndbg/goenv.git ~/.goenv
+	await git clone https://github.com/syndbg/goenv.git $HOME/.goenv &
 fi
 await goenv install 1.20.2 &
 await goenv global 1.20.2 &
-await pip install -r ./requirements.txt
 
-# Install nodejs.
-await curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-await nvm install 16
-
-# Install starship.
-await curl -sS https://starship.rs/install.sh | sh
+# If it's not Termux.
+if [[ -z "${TERMUX_VERSION}" ]]; then
+	ln -sf ~/dotfiles/kitty $HOME/.config/
+	await curl -sS https://starship.rs/install.sh | sh &
+else
+	await pkg i -y getconf &
+	ln -sf ~/dotfiles/.termux $HOME/
+	cp ./patched-fonts/CozetteVector Nerd Font Complete Mono.ttf $HOME/.termux/font.ttf
+	await curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir /data/data/com.termux/files/usr/bin &
+fi
