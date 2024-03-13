1b2a9a85f3b132a313aea10f1d6421140318683b
davemackintosh <me@davemackintosh.co.uk>
2024-03-13 18:13:30 +0000

    Fix PATH in Fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,8 @@ set -U fish_greeting
 if status is-interactive
 	zoxide init fish | source
 	starship init fish | source
-	source $HOME/dotfiles/paths.fish
 	source $HOME/.asdf/asdf.fish
+	source $HOME/dotfiles/paths.fish
 	source $HOME/dotfiles/aliases.fish
 end
 
