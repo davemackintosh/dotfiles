f332e76263dd6b5d9adb14a2290cd4cb4c581c7a
davemackintosh <me@davemackintosh.co.uk>
2024-05-23 11:14:59 +0100

    Not on WSL anymore and hide title bars on Linux

--- exports.fish
+++ exports.fish
@@ -3,7 +3,3 @@ export GPG_TTY="$(tty)"
 export EDITOR=nvim
 export STARSHIP_CONFIG="$HOME/dotfiles/starship.toml"
 
-# If we're in WSL land, we need to use pass as the backend.
-if not test -d "/opt/homebrew/"
-	export AWS_VAULT_BACKEND=pass
-end
